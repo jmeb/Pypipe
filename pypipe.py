deb654a19c43dbb2cf3376c5b922acfd0af9f504
Jacob Barney <barneyj@gmail.com>
2011-07-13 23:17:15 -0500

    Recursion via adding menu and calling pypipe again

--- pypipe.py
+++ pypipe.py
@@ -13,13 +13,18 @@ import os
 
 DIRPRG = "thunar"       #Program to open directories
 FILEPRG = "vlc"         #Program to open files
+PYPIPE = "~/Dropbox/code/python/pypipe.py "
 
-def print_entry(fn,prg):
+def print_file(fn,prg):
     print '<item label="%s">' % fn
     print '\t<action name="Execute">'
     print "\t\t<execute>%s '%s' </execute>" % (prg, os.path.abspath(fn))
     print '\t</action>'
     print '</item>'
+
+def print_dir(src,pypipe):
+    print '<menu id="%s" label="%s" execute="%s" />' % (os.path.abspath(src),
+                                          src,pypipe + os.path.abspath(src))
     
 def recent_file_list(src):
     files = []
@@ -35,6 +40,12 @@ def recent_file_list(src):
     recents = [ t[1] for t in recenttuples ]
     return recents
 
+def printing(recents):
+    for f in recents:
+        if os.path.isdir(os.path.abspath(f)):
+            print_dir(f,PYPIPE) 
+        else:
+            print_file(f,FILEPRG)
 
 def main():
     
@@ -44,11 +55,9 @@ def main():
 
     #The actual output
     print "<openbox_pipe_menu>"
-    for f in recents:
-        if os.path.isdir(os.path.abspath(f)):
-            print_entry(f,DIRPRG)
-        else:
-            print_entry(f,FILEPRG)
+    
+    printing(recents)
+
     print "</openbox_pipe_menu>"
 
 if __name__ == '__main__':
